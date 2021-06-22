920a3a290307af7db07ee670e4806ab0987275ac
viswajit-kuna <viswajit.kuna@HMCTS.NET>
2021-06-22 08:32:33 +0100

    RDCC-2984: - Added more functional tests and supporting test data

--- src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/LrdBuildingLocationsLoadTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/LrdBuildingLocationsLoadTest.java
@@ -18,12 +18,16 @@ import org.springframework.boot.test.context.ConfigFileApplicationContextInitial
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import uk.gov.hmcts.reform.data.ingestion.configuration.AzureBlobConfig;
 import uk.gov.hmcts.reform.data.ingestion.configuration.BlobStorageCredentials;
 import uk.gov.hmcts.reform.locationrefdata.camel.binder.BuildingLocation;
+import uk.gov.hmcts.reform.locationrefdata.camel.binder.ServiceToCcdCaseType;
 import uk.gov.hmcts.reform.locationrefdata.cameltest.testsupport.LrdIntegrationBaseTest;
 import uk.gov.hmcts.reform.locationrefdata.cameltest.testsupport.SpringStarter;
 import uk.gov.hmcts.reform.locationrefdata.config.LrdCamelConfig;
@@ -77,15 +81,96 @@ public class LrdBuildingLocationsLoadTest extends LrdIntegrationBaseTest {
 
     @Test
     @DisplayName("Test for loading a valid CSV file in to a clean building_location table")
-    public void testValidBuildingLocationCSV() throws Exception {
-        testBuildingLocationInsertion();
+    @Sql({"/testData/truncate-building-locations.sql"})
+    public void testLoadValidBuildingLocationCSV() throws Exception {
+        testBuildingLocationInsertion(UPLOAD_FILE_NAME);
     }
 
-    private void testBuildingLocationInsertion() throws Exception {
+    @Test
+    @DisplayName("Test for loading a valid CSV file in to the building_location table "
+        + "which already has a few entries in it")
+    @Sql({"/testData/truncate-building-locations.sql"})
+    public void testAppendBuildingLocation() throws Exception {
+        testBuildingLocationInsertion(UPLOAD_FILE_NAME);
+
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        jdbcTemplate.update("delete from DATALOAD_SCHEDULAR_AUDIT");
+        TransactionStatus status = platformTransactionManager.getTransaction(def);
+        platformTransactionManager.commit(status);
+        SpringStarter.getInstance().restart();
         lrdBlobSupport.uploadFile(
             UPLOAD_FILE_NAME,
             new FileInputStream(getFile(
-                "classpath:sourceFiles/building_location_test.csv"))
+                "classpath:sourceFiles/buildingLocations/building_location_test_2.csv"))
+        );
+        camelContext.getGlobalOptions()
+            .put(SCHEDULER_START_TIME, String.valueOf(new Date(System.currentTimeMillis()).getTime()));
+
+        jobLauncherTestUtils.launchJob();
+
+        validateBuildingLocationFileLoad(ImmutableList.of(
+            BuildingLocation.builder().epimsId("219164")
+                .buildingLocationName("ABERDEEN TRIBUNAL HEARING CENTRE")
+                .postcode("AB116LT")
+                .address("AB1, 48 HUNTLY STREET, ABERDEEN")
+                .build(),
+            BuildingLocation.builder().epimsId("827534")
+                .buildingLocationName("ABERYSTWYTH JUSTICE CENTRE")
+                .postcode("SY231AS")
+                .address("TREFECHANY Lanfa  Trefechan  Aberystwyth")
+                .build(),
+            BuildingLocation.builder().epimsId("450049")
+                .buildingLocationName("ALDERSHOT JUSTICE CENTRE")
+                .postcode("GU111NY")
+                .address("THE COURT HOUSE, CIVIC CENTRE, WELLINGTON AVENUE")
+                .build(),
+            BuildingLocation.builder().epimsId("364992")
+                .buildingLocationName("ALDGATE TOWER (3RD FLOOR)")
+                .postcode("E1 8FA")
+                .address("3rd Floor Aldgate Tower 2 Leman Street, LONDON")
+                .build()
+        ), 4);
+        validateLrdServiceFileAudit(jdbcTemplate, auditSchedulerQuery,
+                                    "Success", "building_location_test_2.csv");
+        lrdBlobSupport.deleteBlob("building_location_test_2.csv");
+    }
+
+    @Test
+    @DisplayName("Test for loading a valid CSV file which has data not enclosed within quotes"
+        + " in to a clean building_location table")
+    @Sql({"/testData/truncate-building-locations.sql"})
+    public void testLoadValidBuildingLocationCSVWithoutQuotes() throws Exception {
+        testBuildingLocationInsertion("building_location_test_without_quotes.csv");
+    }
+
+    @Test
+    @DisplayName("Test for loading a valid CSV file which has entries missing a few non-mandatory fields")
+    @Sql({"/testData/truncate-building-locations.sql"})
+    public void testBuildingLocationCSV_WithNoRegionAndCluster() throws Exception {
+        testBuildingLocationInsertion("building_location_test_no_region_cluster.csv");
+    }
+
+    @Test
+    @DisplayName("Test for loading a valid CSV file which has a combination of valid entries and entries missing"
+        + " a mandatory field")
+    @Sql({"/testData/truncate-building-locations.sql"})
+    public void testLoadValidBuildingLocationCSV_WithMissingMandatoryValue() throws Exception {
+        testBuildingLocationInsertion("building_location_test_no_postcode.csv");
+    }
+
+    @Test
+    @DisplayName("Test for loading a valid CSV file which has a combination of valid entries and entries missing"
+        + " the primary key")
+    @Sql({"/testData/truncate-building-locations.sql"})
+    public void testLoadValidBuildingLocationCSV_WithMissingEpimsId() throws Exception {
+        testBuildingLocationInsertion("building_location_test_no_epims_id.csv");
+    }
+
+    private void testBuildingLocationInsertion(String fileName) throws Exception {
+        lrdBlobSupport.uploadFile(
+            UPLOAD_FILE_NAME,
+            new FileInputStream(getFile(
+                String.format("classpath:sourceFiles/buildingLocations/%s.csv", fileName)))
         );
 
         jobLauncherTestUtils.launchJob();
@@ -103,9 +188,9 @@ public class LrdBuildingLocationsLoadTest extends LrdIntegrationBaseTest {
                 .build()
         ), 2);
         //Validates Success Audit
-        validateLrdServiceFileAudit(jdbcTemplate, auditSchedulerQuery, "Success", UPLOAD_FILE_NAME);
+        validateLrdServiceFileAudit(jdbcTemplate, auditSchedulerQuery, "Success", fileName);
         //Delete Uploaded test file with Snapshot delete
-        lrdBlobSupport.deleteBlob(UPLOAD_FILE_NAME);
+        lrdBlobSupport.deleteBlob(fileName);
     }
 
     private void validateBuildingLocationFileLoad(List<BuildingLocation> expectedBuildingLocationList, int size) {
---
+++ src/functionalTest/resources/testData/truncate-building-locations.sql
@@ -0,0 +1,4 @@
+truncate building_location;
+truncate DATALOAD_SCHEDULAR_AUDIT;
+truncate DATALOAD_EXCEPTION_RECORDS;
+commit;

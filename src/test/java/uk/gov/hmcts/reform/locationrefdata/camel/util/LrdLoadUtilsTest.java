dbb82fe065b2015cab6055b8aba860707d20d07d
viswajit-kuna <viswajit.kuna@HMCTS.NET>
2021-06-23 09:04:53 +0100

    RDCC-2984: - Added additional scenario in the LrdLoadUtilsTest class

--- src/test/java/uk/gov/hmcts/reform/locationrefdata/camel/util/LrdLoadUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/locationrefdata/camel/util/LrdLoadUtilsTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LrdLoadUtilsTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {" abc", "abc ", " abc "})
+    @ValueSource(strings = {" abc", "abc ", " abc ", "abc"})
     public void testTrim(String value) {
         assertThat(LrdLoadUtils.trim(value))
             .isNotBlank()

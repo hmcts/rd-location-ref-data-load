b2a0a1bfb8250933db350899c4e0b4d122d16fb4
ishratt <tunazzina.ishrat@hmcts.net>
2021-06-30 17:22:27 +0100

    RDCC-2985_1: minor change to re-trigger build

--- src/functionalTest/resources/db/testmigration/V1_12__building_location_test_insert.sql
+++ src/functionalTest/resources/db/testmigration/V1_12__building_location_test_insert.sql
@@ -1,1 +1,1 @@
-insert into building_location (address,building_location_name,epimms_id,postcode) values ('some address', 'aberdeen', '123456', 'ABC 123')
+INSERT INTO building_location (address,building_location_name,epimms_id,postcode) VALUES ('some address', 'aberdeen', '123456', 'ABC 123');

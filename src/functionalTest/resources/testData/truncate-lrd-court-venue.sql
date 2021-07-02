673c3509125109140f7a0a97857a3ecfcfe794c1
ishratt <tunazzina.ishrat@hmcts.net>
2021-07-02 11:53:12 +0100

    RDCC-2985_1: minor refactor

--- src/functionalTest/resources/testData/truncate-lrd-court-venue.sql
+++ src/functionalTest/resources/testData/truncate-lrd-court-venue.sql
@@ -2,5 +2,4 @@ truncate COURT_VENUE cascade;
 truncate BUILDING_LOCATION cascade;
 truncate DATALOAD_SCHEDULAR_AUDIT;
 truncate DATALOAD_EXCEPTION_RECORDS;
-insert into BUILDING_LOCATION (address,building_location_name,epimms_id,postcode) VALUES ('some address', 'aberdeen', '123456', 'ABC 123');
 commit;

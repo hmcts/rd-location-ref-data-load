2b6e1f4d766ab2e0f7d84c1231dca54146a891c8
ishratt <tunazzina.ishrat@hmcts.net>
2021-07-02 10:48:33 +0100

    RDCC-2985_1: fixing sql script issue

--- src/functionalTest/resources/testData/truncate-lrd-court-venue.sql
+++ src/functionalTest/resources/testData/truncate-lrd-court-venue.sql
@@ -1,5 +1,6 @@
-truncate court_venue cascade;
-truncate dataload_schedular_audit;
-truncate dataload_exception_records;
-insert into building_location (address,building_location_name,epimms_id,postcode) values ('some address', 'aberdeen', '123456', 'ABC 123')
+truncate COURT_VENUE cascade;
+truncate BUILDING_LOCATION cascade;
+truncate DATALOAD_SCHEDULAR_AUDIT;
+truncate DATALOAD_EXCEPTION_RECORDS;
+insert into BUILDING_LOCATION (address,building_location_name,epimms_id,postcode) VALUES ('some address', 'aberdeen', '123456', 'ABC 123');
 commit;

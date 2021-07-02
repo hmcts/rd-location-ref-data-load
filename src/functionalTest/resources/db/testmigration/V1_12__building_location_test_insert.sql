9e3d742bbe423b257bed0ae240286e0b0df2c68b
ishratt <tunazzina.ishrat@hmcts.net>
2021-07-02 10:02:42 +0100

    RDCC-2985_1: functional test failure fix

--- src/functionalTest/resources/db/testmigration/V1_12__building_location_test_insert.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO building_location (address,building_location_name,epimms_id,postcode) VALUES ('some address', 'aberdeen', '123456', 'ABC 123');
--- src/functionalTest/resources/testData/truncate-lrd-court-venue.sql
+++ src/functionalTest/resources/testData/truncate-lrd-court-venue.sql
@@ -1,4 +1,5 @@
 truncate court_venue cascade;
 truncate dataload_schedular_audit;
 truncate dataload_exception_records;
+insert into building_location (address,building_location_name,epimms_id,postcode) values ('some address', 'aberdeen', '123456', 'ABC 123')
 commit;

721abfe246ae71165bb67edf655ce677a42d34a7
AdilOoze <adil.oozeerally@hmcts.net>
2021-07-19 17:18:40 +0100

    RDCC-3075: removing @EqualsAndHashCode from BuildingLocation and adding Unit Tests

--- src/main/java/uk/gov/hmcts/reform/locationrefdata/camel/binder/BuildingLocation.java
+++ src/main/java/uk/gov/hmcts/reform/locationrefdata/camel/binder/BuildingLocation.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.locationrefdata.camel.binder;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -21,7 +20,6 @@ import javax.validation.constraints.Pattern;
 @Builder
 @RequiredArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 public class BuildingLocation {
 
     @DataField(pos = 1, columnName = "ePIMS_ID")

bde8c2c4901eabcef4e4e0d0866acc99bbcbbdcf
viswajit-kuna <viswajit.kuna@HMCTS.NET>
2021-07-23 14:46:49 +0100

    corrected the file name to be deleted

--- src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/LrdBuildingLocationFileStatusCheck.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/LrdBuildingLocationFileStatusCheck.java
@@ -173,7 +173,7 @@ public class LrdBuildingLocationFileStatusCheck extends LrdIntegrationBaseTest {
     @AfterEach
     void tearDown() throws Exception {
         //Delete Uploaded test file with Snapshot delete
-        if (lrdBlobSupport.isBlobPresent(UPLOAD_ORG_SERVICE_FILE_NAME)) {
+        if (lrdBlobSupport.isBlobPresent(UPLOAD_FILE_NAME)) {
             lrdBlobSupport.deleteBlob(UPLOAD_FILE_NAME);
         }
     }

91a8b6cc8a78ff8202e74caf8b66359c4ddfc883
shivajilokare <shivaji.lokare@HCTS.NET>
2022-02-16 14:42:21 +0000

    script to update sscs

---
+++ src/main/resources/db/migration/V1_15_insert_SSCS_record.sql
@@ -0,0 +1,3 @@
+INSERT INTO judicial_location_mapping (judicial_base_location_id,base_location_name,epimms_id,building_location_name, service_code) VALUES ('1032','Social Entitlement','','','BBA3');
+
+COMMIT;

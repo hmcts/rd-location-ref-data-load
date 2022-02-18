f38dbfe90957c90b8834669a20e61185238299cc
shivajilokare <shivaji.lokare@HCTS.NET>
2022-02-18 12:27:51 +0000

    added script to delete

--- src/main/resources/db/migration/V1_16_Update_service_ticket_code_for_sscs.sql
+++ src/main/resources/db/migration/V1_16_Update_service_ticket_code_for_sscs.sql
@@ -1,3 +1,3 @@
-DELETE FROM judicial_service_code_mapping WHERE service_id ='21' OR service_id ='55'
-DELETE FROM  judicial_ticket_code_mapping WHERE ticket_code ='363' OR ticket_code ='376'
+DELETE FROM judicial_service_code_mapping WHERE service_id ='21' OR service_id ='55';
+DELETE FROM  judicial_ticket_code_mapping WHERE ticket_code ='363' OR ticket_code ='376';
 COMMIT;

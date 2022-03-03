5b7cd9c1c026f1c6a4fc3145c9a6788f097bb3e3
LukeHalsall <luke.halsall@HMCTS.net>
2022-03-03 11:20:22 +0000

    changed to lowercase

--- src/functionalTest/resources/db/testmigration/V1_15__alter_tables_add_columns.sql
+++ src/functionalTest/resources/db/testmigration/V1_15__alter_tables_add_columns.sql
@@ -19,27 +19,26 @@ ALTER TABLE building_location ADD COLUMN mrd_created_time TIMESTAMP;
 ALTER TABLE building_location ADD COLUMN mrd_updated_time TIMESTAMP;
 ALTER TABLE building_location ADD COLUMN mrd_deleted_time TIMESTAMP;
 
-ALTER TABLE court_type ADD COLUMN MRD_Created_Time TIMESTAMP;
-ALTER TABLE court_type ADD COLUMN MRD_Updated_Time TIMESTAMP;
-ALTER TABLE court_type ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+ALTER TABLE court_type ADD COLUMN mrd_created_time TIMESTAMP;
+ALTER TABLE court_type ADD COLUMN mrd_updated_time TIMESTAMP;
+ALTER TABLE court_type ADD COLUMN mrd_deleted_time TIMESTAMP;
 
-ALTER TABLE region ADD COLUMN MRD_Created_Time TIMESTAMP;
-ALTER TABLE region ADD COLUMN MRD_Updated_Time TIMESTAMP;
-ALTER TABLE region ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+ALTER TABLE region ADD COLUMN mrd_created_time TIMESTAMP;
+ALTER TABLE region ADD COLUMN mrd_updated_time TIMESTAMP;
+ALTER TABLE region ADD COLUMN mrd_deleted_time TIMESTAMP;
 
-ALTER TABLE cluster ADD COLUMN MRD_Created_Time TIMESTAMP;
-ALTER TABLE cluster ADD COLUMN MRD_Updated_Time TIMESTAMP;
-ALTER TABLE cluster ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+ALTER TABLE cluster ADD COLUMN mrd_created_time TIMESTAMP;
+ALTER TABLE cluster ADD COLUMN mrd_updated_time TIMESTAMP;
+ALTER TABLE cluster ADD COLUMN mrd_deleted_time TIMESTAMP;
 
-ALTER TABLE court_type_service_assoc ADD COLUMN MRD_Created_Time TIMESTAMP;
-ALTER TABLE court_type_service_assoc ADD COLUMN MRD_Updated_Time TIMESTAMP;
-ALTER TABLE court_type_service_assoc ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+ALTER TABLE court_type_service_assoc ADD COLUMN mrd_created_time TIMESTAMP;
+ALTER TABLE court_type_service_assoc ADD COLUMN mrd_updated_time TIMESTAMP;
+ALTER TABLE court_type_service_assoc ADD COLUMN mrd_deleted_time TIMESTAMP;
 
-ALTER TABLE district_civil_jurisdiction ADD COLUMN MRD_Created_Time TIMESTAMP;
-ALTER TABLE district_civil_jurisdiction ADD COLUMN MRD_Updated_Time TIMESTAMP;
-ALTER TABLE district_civil_jurisdiction ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+ALTER TABLE district_civil_jurisdiction ADD COLUMN mrd_created_time TIMESTAMP;
+ALTER TABLE district_civil_jurisdiction ADD COLUMN mrd_updated_time TIMESTAMP;
+ALTER TABLE district_civil_jurisdiction ADD COLUMN mrd_deleted_time TIMESTAMP;
 
-ALTER TABLE district_family_jurisdiction ADD COLUMN MRD_Created_Time TIMESTAMP;
-ALTER TABLE district_family_jurisdiction ADD COLUMN MRD_Updated_Time TIMESTAMP;
-ALTER TABLE district_family_jurisdiction ADD COLUMN MRD_Deleted_Time TIMESTAMP;
-
+ALTER TABLE district_family_jurisdiction ADD COLUMN mrd_created_time TIMESTAMP;
+ALTER TABLE district_family_jurisdiction ADD COLUMN mrd_updated_time TIMESTAMP;
+ALTER TABLE district_family_jurisdiction ADD COLUMN mrd_deleted_time TIMESTAMP;

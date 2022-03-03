0d553c03d2e6032fc69a101884fd03f41d8b3359
LukeHalsall <luke.halsall@HMCTS.net>
2022-03-03 11:00:29 +0000

    merged changes

--- src/main/resources/db/migration/V1_15__alter_tables_add_columns.sql
+++ src/main/resources/db/migration/V1_15__alter_tables_add_columns.sql
@@ -18,3 +18,28 @@ ALTER TABLE building_location ADD COLUMN mrd_building_location_id VARCHAR(16);
 ALTER TABLE building_location ADD COLUMN mrd_created_time TIMESTAMP;
 ALTER TABLE building_location ADD COLUMN mrd_updated_time TIMESTAMP;
 ALTER TABLE building_location ADD COLUMN mrd_deleted_time TIMESTAMP;
+
+ALTER TABLE court_type ADD COLUMN MRD_Created_Time TIMESTAMP;
+ALTER TABLE court_type ADD COLUMN MRD_Updated_Time TIMESTAMP;
+ALTER TABLE court_type ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+
+ALTER TABLE region ADD COLUMN MRD_Created_Time TIMESTAMP;
+ALTER TABLE region ADD COLUMN MRD_Updated_Time TIMESTAMP;
+ALTER TABLE region ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+
+ALTER TABLE cluster ADD COLUMN MRD_Created_Time TIMESTAMP;
+ALTER TABLE cluster ADD COLUMN MRD_Updated_Time TIMESTAMP;
+ALTER TABLE cluster ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+
+ALTER TABLE court_type_service_assoc ADD COLUMN MRD_Created_Time TIMESTAMP;
+ALTER TABLE court_type_service_assoc ADD COLUMN MRD_Updated_Time TIMESTAMP;
+ALTER TABLE court_type_service_assoc ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+
+ALTER TABLE district_civil_jurisdiction ADD COLUMN MRD_Created_Time TIMESTAMP;
+ALTER TABLE district_civil_jurisdiction ADD COLUMN MRD_Updated_Time TIMESTAMP;
+ALTER TABLE district_civil_jurisdiction ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+
+ALTER TABLE district_family_jurisdiction ADD COLUMN MRD_Created_Time TIMESTAMP;
+ALTER TABLE district_family_jurisdiction ADD COLUMN MRD_Updated_Time TIMESTAMP;
+ALTER TABLE district_family_jurisdiction ADD COLUMN MRD_Deleted_Time TIMESTAMP;
+

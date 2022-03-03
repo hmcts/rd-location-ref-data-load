48af8d23081af16b4b57b94b1519b1604d4c102c
AdilOoze <adil.oozeerally@hmcts.net>
2022-03-03 09:59:56 +0000

    RDCC-4131: merging changes from RDCC-4131 branch to develop branch

--- src/main/resources/db/migration/V1_15__alter_tables_add_columns.sql
+++ src/main/resources/db/migration/V1_15__alter_tables_add_columns.sql
@@ -8,3 +8,13 @@ ALTER TABLE court_venue ADD COLUMN fact_url VARCHAR(1024);
 ALTER TABLE court_venue ADD COLUMN mrd_created_time TIMESTAMP;
 ALTER TABLE court_venue ADD COLUMN mrd_updated_time TIMESTAMP;
 ALTER TABLE court_venue ADD COLUMN mrd_deleted_time TIMESTAMP;
+
+ALTER TABLE building_location ADD COLUMN welsh_building_location_name VARCHAR(256);
+ALTER TABLE building_location ADD COLUMN welsh_address VARCHAR(512);
+ALTER TABLE building_location ADD COLUMN uprn VARCHAR(16);
+ALTER TABLE building_location ADD COLUMN latitude DECIMAL(10,8);
+ALTER TABLE building_location ADD COLUMN longitude DECIMAL(11,8);
+ALTER TABLE building_location ADD COLUMN mrd_building_location_id VARCHAR(16);
+ALTER TABLE building_location ADD COLUMN mrd_created_time TIMESTAMP;
+ALTER TABLE building_location ADD COLUMN mrd_updated_time TIMESTAMP;
+ALTER TABLE building_location ADD COLUMN mrd_deleted_time TIMESTAMP;

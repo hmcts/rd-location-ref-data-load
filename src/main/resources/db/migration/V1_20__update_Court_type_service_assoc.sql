b92c3f9dda0438d9afbc1b4b3f8d96635f4dd4e4
shivajilokare <shivaji.lokare@HCTS.NET>
2022-09-14 14:32:05 +0100

    fix

--- src/main/resources/db/migration/V1_20__update_Court_type_service_assoc.sql
+++ src/main/resources/db/migration/V1_20__update_Court_type_service_assoc.sql
@@ -1,3 +1,3 @@
  INSERT INTO court_type_service_assoc (court_type_service_assoc_id,service_code, court_type_id, mrd_created_time, mrd_updated_time, mrd_deleted_time) VALUES
- (23,'ABA4', '18', '05/09/2022 02:00', '05/09/2022 02:00', NULL);
+ (23,'ABA4', '18', '05/09/2022 02:00:00', '05/09/2022 02:00:00', NULL);
  COMMIT;

a49653b492adb9cbfc305c217c7b532f9efd6f80
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-05-26 11:54:41 +0100

    [RDCC-6746]-upgrade deprecated methods to java 17

--- src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/testsupport/LrdIntegrationBaseTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/testsupport/LrdIntegrationBaseTest.java
@@ -211,6 +211,6 @@ public abstract class LrdIntegrationBaseTest {
 
 
     protected Timestamp getTime(String sql, String serviceCode, String caseType) {
-        return jdbcTemplate.queryForObject(sql, new Object[]{serviceCode, caseType}, Timestamp.class);
+        return jdbcTemplate.queryForObject(sql, Timestamp.class, new Object[]{serviceCode, caseType});
     }
 }

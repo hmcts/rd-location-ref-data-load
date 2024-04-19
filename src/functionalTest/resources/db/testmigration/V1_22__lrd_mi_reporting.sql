ec3ae46ea42f6f57b492143c42733228c68eb910
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-04-19 13:56:19 +0100

    DTSRD-2505: sync both ref-api and datalod reposiories

--- src/functionalTest/resources/db/testmigration/V1_22__lrd_mi_reporting.sql
+++ src/functionalTest/resources/db/testmigration/V1_22__lrd_mi_reporting.sql
@@ -2,25 +2,25 @@ CREATE SCHEMA IF NOT EXISTS rdlocationreport;
 
 CREATE OR REPLACE VIEW rdlocationreport.vw_org_unit AS
 SELECT org_unit_id, description
-FROM locrefdata.org_unit;
+FROM org_unit;
 
 CREATE OR REPLACE VIEW rdlocationreport.vw_org_business_area AS
 SELECT business_area_id, description
-FROM locrefdata.org_business_area;
+FROM org_business_area;
 
 CREATE OR REPLACE VIEW rdlocationreport.vw_org_sub_business_area AS
 SELECT sub_business_area_id, description
-FROM locrefdata.org_sub_business_area;
+FROM org_sub_business_area;
 
 CREATE OR REPLACE VIEW rdlocationreport.vw_jurisdiction AS
 SELECT jurisdiction_id, description
-FROM locrefdata.jurisdiction;
+FROM jurisdiction;
 
 CREATE OR REPLACE VIEW rdlocationreport.vw_service AS
 SELECT service_id, org_unit_id, business_area_id, sub_business_area_id, jurisdiction_id,service_code,service_description, service_short_description
-FROM locrefdata.service;
+FROM service;
 
 CREATE OR REPLACE VIEW rdlocationreport.vw_region AS
 SELECT region_id, description
-FROM locrefdata.region;
+FROM region;
 

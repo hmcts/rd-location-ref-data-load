d392aa118e37785045dcd24ea35d73007fd47f35
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-04-19 15:59:59 +0100

    DTSRD-2505: remove migration files, migration has to be from location-ref-api

--- src/main/resources/db/migration/V1_24__drop_indexes.sql
+++
@@ -1,7 +0,0 @@
-DROP INDEX IF EXISTS service_code_idx ;
-
-
-DROP INDEX  IF EXISTS ccd_case_type_idx ;
-
-
-
--- src/main/resources/db/migration/V1_25__alter_tables_add_columns.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE court_venue ADD COLUMN IF NOT EXISTS short_court_name VARCHAR(80);

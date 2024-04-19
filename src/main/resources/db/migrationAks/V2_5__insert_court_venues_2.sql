3160f7a6a19d904c796bbe729c919e8bda7223d5
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-04-19 13:40:16 +0100

    DTSRD-2505: sync both ref-api and datalod reposiories

--- src/main/resources/db/migrationAks/V2_5__insert_court_venues_2.sql
+++
@@ -1,3 +0,0 @@
-insert into court_venue (court_venue_id, epimms_id, site_name, created_time, updated_time, region_id, court_type_id, cluster_id, open_for_public, court_address, postcode, phone_number, closed_date, court_location_code, dx_address, welsh_site_name, welsh_court_address, court_status, court_open_date, court_name, is_case_management_location, is_hearing_location, is_temporary_location, location_type)
-values(11, '815833', 'Stoke-on-Trent Combined Court', '2022-07-05 09:50:46.269032','2022-07-05 09:50:46.269032','7','23','8',true,'BETHESDA STREET','ST1 3BP',null,null,null,null,null,null,'Open', null,'STOKE-ON-TRENT COMBINED COURT', 'Y', 'Y', 'N', 'NBC');
-

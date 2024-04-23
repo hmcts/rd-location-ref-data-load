25949bf9bfe92a518eb401157e6ef76ce75af793
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2024-04-23 09:00:48 +0100

    Dtsrd 2540 (#403)
    * DTSRD-2505: Add new column (Short_Court_Name) to the court_venue table
    
    * DTSRD-2505: Add new column (Short_Court_Name) to the court_venue table
    
    * DTSRD-2505: revert the changes done for Account names
    
    * DTSRD-2505: revert the changes done for Account names
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: sync both ref-api and datalod reposiories
    
    * DTSRD-2505: remove migration files, migration has to be from location-ref-api
    
    * DTSRD-2505: Flyway migration should be done from rd-location-ref-api

---
+++ src/functionalTest/resources/db/testmigration/V1_22__lrd_mi_reporting.sql
@@ -0,0 +1,26 @@
+CREATE SCHEMA IF NOT EXISTS rdlocationreport;
+
+CREATE OR REPLACE VIEW rdlocationreport.vw_org_unit AS
+SELECT org_unit_id, description
+FROM org_unit;
+
+CREATE OR REPLACE VIEW rdlocationreport.vw_org_business_area AS
+SELECT business_area_id, description
+FROM org_business_area;
+
+CREATE OR REPLACE VIEW rdlocationreport.vw_org_sub_business_area AS
+SELECT sub_business_area_id, description
+FROM org_sub_business_area;
+
+CREATE OR REPLACE VIEW rdlocationreport.vw_jurisdiction AS
+SELECT jurisdiction_id, description
+FROM jurisdiction;
+
+CREATE OR REPLACE VIEW rdlocationreport.vw_service AS
+SELECT service_id, org_unit_id, business_area_id, sub_business_area_id, jurisdiction_id,service_code,service_description, service_short_description
+FROM service;
+
+CREATE OR REPLACE VIEW rdlocationreport.vw_region AS
+SELECT region_id, description
+FROM region;
+
---
+++ src/functionalTest/resources/db/testmigration/V1_24__drop_indexes.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS service_code_idx ;
+
+
+DROP INDEX IF EXISTS ccd_case_type_idx ;
+
+
+

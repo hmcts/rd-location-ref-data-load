3474a9b37e226d555dc81d9f128de4bbf04cdaff
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2024-09-04 14:13:01 +0100

    DTSRD-722 (#497)
    * removed duplication
    
    * checkstyle

--- src/main/java/uk/gov/hmcts/reform/locationrefdata/camel/mapper/BuildingLocationMapper.java
+++ src/main/java/uk/gov/hmcts/reform/locationrefdata/camel/mapper/BuildingLocationMapper.java
@@ -3,13 +3,10 @@ package uk.gov.hmcts.reform.locationrefdata.camel.mapper;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.data.ingestion.camel.mapper.IMapper;
 import uk.gov.hmcts.reform.locationrefdata.camel.binder.BuildingLocation;
-import uk.gov.hmcts.reform.locationrefdata.camel.util.LrdLoadUtils;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.locationrefdata.camel.util.LrdLoadUtils.trim;
-import static uk.gov.hmcts.reform.locationrefdata.camel.util.LrdLoadUtils.trimNumeric;
 
 @Component
 public class BuildingLocationMapper implements IMapper {
@@ -17,35 +14,29 @@ public class BuildingLocationMapper implements IMapper {
     @Override
     public Map<String, Object> getMap(Object buildingLocationObj) {
         BuildingLocation buildingLocation = (BuildingLocation) buildingLocationObj;
-        Map<String, Object> buildingLocationParamMap = new HashMap<>();
-        buildingLocationParamMap.put("epimms_id", trim(buildingLocation.getEpimmsId()));
-        buildingLocationParamMap.put("building_location_name", trim(buildingLocation.getBuildingLocationName()));
-        buildingLocationParamMap.put("building_location_status", trim(buildingLocation.getBuildingLocationStatus()));
-        buildingLocationParamMap.put("area", trim(buildingLocation.getArea()));
-        buildingLocationParamMap.put("region_id", trimNumeric(buildingLocation.getRegionId()));
-        buildingLocationParamMap.put("cluster_id", trimNumeric(buildingLocation.getClusterId()));
-        buildingLocationParamMap.put("court_finder_url", trim(buildingLocation.getCourtFinderUrl()));
-        buildingLocationParamMap.put("postcode", trim(buildingLocation.getPostcode()));
-        buildingLocationParamMap.put("address", trim(buildingLocation.getAddress()));
-        buildingLocationParamMap.put("welsh_building_location_name",
+
+        Map<String, Object> map = CommonMapper.getMap(buildingLocation.getEpimmsId(),
+                                                       buildingLocation.getRegionId(),
+                                                       buildingLocation.getClusterId(),
+                                                       buildingLocation.getPostcode(),
+                                                       buildingLocation.getUprn(),
+                                                       buildingLocation.getMrdBuildingLocationId(),
+                                                       buildingLocation.getMrdCreatedTime(),
+                                                       buildingLocation.getMrdUpdatedTime(),
+                                                       buildingLocation.getMrdDeletedTime());
+
+        map.put("building_location_name", trim(buildingLocation.getBuildingLocationName()));
+        map.put("building_location_status", trim(buildingLocation.getBuildingLocationStatus()));
+        map.put("area", trim(buildingLocation.getArea()));
+        map.put("court_finder_url", trim(buildingLocation.getCourtFinderUrl()));
+        map.put("address", trim(buildingLocation.getAddress()));
+        map.put("welsh_building_location_name",
                                      trim(buildingLocation.getWelshBuildingLocationName()));
-        buildingLocationParamMap.put("welsh_address",
-                                     trim(buildingLocation.getWelshAddress()));
-        buildingLocationParamMap.put("uprn",
-                                     trim(buildingLocation.getUprn()));
-        buildingLocationParamMap.put("latitude",
-                                     buildingLocation.getLatitude());
-        buildingLocationParamMap.put("longitude",
-                                     buildingLocation.getLongitude());
-        buildingLocationParamMap.put("mrd_building_location_id",
-                                     trimNumeric(buildingLocation.getMrdBuildingLocationId()));
-        buildingLocationParamMap.put("mrd_created_time",
-                                     LrdLoadUtils.getDateTimeStamp(buildingLocation.getMrdCreatedTime()));
-        buildingLocationParamMap.put("mrd_updated_time",
-                                     LrdLoadUtils.getDateTimeStamp(buildingLocation.getMrdUpdatedTime()));
-        buildingLocationParamMap.put("mrd_deleted_time",
-                                     LrdLoadUtils.getDateTimeStamp(buildingLocation.getMrdDeletedTime()));
-        return buildingLocationParamMap;
+        map.put("welsh_address", trim(buildingLocation.getWelshAddress()));
+        map.put("latitude", buildingLocation.getLatitude());
+        map.put("longitude", buildingLocation.getLongitude());
+
+        return map;
     }
 
 }
---
+++ src/main/java/uk/gov/hmcts/reform/locationrefdata/camel/mapper/CommonMapper.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.locationrefdata.camel.mapper;
+
+import uk.gov.hmcts.reform.locationrefdata.camel.util.LrdLoadUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.locationrefdata.camel.util.LrdLoadUtils.trimNumeric;
+
+public class CommonMapper {
+
+    private CommonMapper() {
+        // utility class
+    }
+
+    public static Map<String, Object> getMap(String epimmsId, String regionId, String clusterId,
+                                             String postcode, String uprn, String locationId, String mrdCreatedTime,
+                                             String mrdUpdatedTime, String mrdDeletedTime) {
+        Map<String, Object> commonMapping = new HashMap<>();
+
+        commonMapping.put("epimms_id", LrdLoadUtils.trim(epimmsId));
+        commonMapping.put("region_id", trimNumeric(regionId));
+        commonMapping.put("cluster_id", trimNumeric(clusterId));
+        commonMapping.put("postcode", LrdLoadUtils.trim(postcode));
+        commonMapping.put("uprn", LrdLoadUtils.trim(uprn));
+        commonMapping.put("mrd_building_location_id", trimNumeric(locationId));
+        commonMapping.put("mrd_created_time", LrdLoadUtils.getDateTimeStamp(mrdCreatedTime));
+        commonMapping.put("mrd_updated_time", LrdLoadUtils.getDateTimeStamp(mrdUpdatedTime));
+        commonMapping.put("mrd_deleted_time", LrdLoadUtils.getDateTimeStamp(mrdDeletedTime));
+
+        return commonMapping;
+    }
+}

b03e2ccf8e2dc408e7d66c80f024565e13b49494
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2024-10-01 16:23:18 +0100

    DTSRD-3404: Upgrade Location Data load to Spring boot 3

--- src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/testsupport/LrdBlobSupport.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/locationrefdata/cameltest/testsupport/LrdBlobSupport.java
@@ -56,7 +56,7 @@ public class LrdBlobSupport {
 
     public void uploadFile(String blob, InputStream sourceFile) throws Exception {
         BlobClient cloudBlockBlob = cloudBlobContainer.getBlobClient(blob);
-        cloudBlockBlob.upload(sourceFile, 8 * 1024 * 1024);
+        cloudBlockBlob.upload(sourceFile);
     }
 
     public void deleteBlob(String blob, boolean... status) throws Exception {

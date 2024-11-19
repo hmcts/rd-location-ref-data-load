294991d1cf3073669014be7302be989de83f80bc
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2024-11-19 10:22:20 +0000

    Update azurerm to 4.10 (#538)

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.106"
+      version = "~> 4.10"
     }
     azuread = {
       source  = "hashicorp/azuread"
